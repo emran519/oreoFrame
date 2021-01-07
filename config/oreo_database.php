36dea59b7d9b140b0aa8fbd49dfa047fb8435679
Oreo <609451870@qq.com>
2021-01-07 23:27:05 +0800

    OreoFrame微PHP快速开发框架

--- config/oreo_database.php
+++ config/oreo_database.php
@@ -7,15 +7,15 @@ return [
     // 数据库驱动,目前支持mysql
 	"type" => "mysql",
     // 数据库账号
-	"user" => "e_dev_2free_cn",
+	"user" => "",
     // 数据库库名
-    "database" => "e_dev_2free_cn",
+    "database" => "",
 	// 数据库密码
-    "password" => "t5fX3ZGrLe5fZxKw",
+    "password" => "",
 	// 数据库编码默认采用utf8
     "charset" => "utf8",
     // 数据库表前缀
-	"prefix" => "oreo_",
+	"prefix" => "",
     // 是否使用长连接
     "connect" => false,
 ];
